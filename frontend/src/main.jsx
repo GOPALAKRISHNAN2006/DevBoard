d8a66f53ae22b1ce0297b90d52335fc689923024
unknown <gopalmuruga007@gmail.com>
2026-08-18 22:41:28 +0530

    Fix blank page issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,12 @@
-import { StrictMode } from 'react'; 
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'; import App from './App'; 
-  
-createRoot(document.getElementById('root')).render(
-<StrictMode><App/></StrictMode>);
+import './index.css';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
